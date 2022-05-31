1a24b22760ffedfe095db8c8084da0309a098604
aniruddh-gohil <103560025+aniruddh-gohil@users.noreply.github.com>
2022-05-31 19:59:54 +0530

    Test case completed

--- test/stages/stage2-vnet-peering2to1.tf
+++
@@ -1,4 +0,0 @@
-#module for Azure-Vnet-Peering
-module "azure-vnet-peering" {
-  source = "/Users/aniruddhgohil/Documents/GitHub/terraform-azure-vnet-peering"
-}
--- test/stages/variables.tf
+++ test/stages/variables.tf
@@ -1,12 +1,21 @@
+
 # Resource Group Variables
 variable "resource_group_name" {
   type        = string
   description = "Existing resource group where the IKS cluster will be provisioned."
 }
+
 variable "region" {
   type        = string
   description = "Region for VLANs defined in private_vlan_number and public_vlan_number."
 }
+
+variable "name_prefix" {
+  type        = string
+  description = "Prefix name that should be used for the cluster and services. If not provided then resource_group_name will be used"
+  default     = ""
+}
+
 variable "enabled" {}
 
 variable "subscription_id" {}
