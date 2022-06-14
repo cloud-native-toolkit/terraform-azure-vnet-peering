7de764a58bf047ddceecc42a2bca4689470bc614
hemang-ibm <103431190+hemang-ibm@users.noreply.github.com>
2022-06-14 12:58:36 +0530

    Update main.tf
    resource group creation script removed from main.tf
    
    Signed-off-by: aniruddh-gohil <103560025+aniruddh-gohil@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-
-#resource "azurerm_virtual_network" "virtual_network" {
-# name                = var.virtual_network_name
-#location            = var.region
-#resource_group_name = var.resource_group_name
-#address_space       = var.virtual_network_address_space
-#}
-
-resource "azurerm_resource_group" "resource_group" {
-  name     = var.resource_group_name
-  location = var.region
-}
-
 resource "azurerm_virtual_network" "edgevnet" {
   name                = var.virtual_network_name_edge
   resource_group_name = var.resource_group_name
